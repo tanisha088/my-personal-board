287aacc15becb93b1e6855c4b6f4ae9052019da9
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-19 15:50:31 +0530

    Update zoomin.js

--- docs/js/zoomin.js
+++ docs/js/zoomin.js
@@ -4,12 +4,12 @@ const zoomout=document.querySelector(".zoom-out");
 zoomin.addEventListener("click",function(e){
 
     ctx.scale(1.1,1.1);
-    ctx.translate(-50,-0);
+   // ctx.translate(-50,0);
     redraw();
 })
 zoomout.addEventListener("click",function(){
     
     ctx.scale(0.9,0.9);
-    ctx.translate(50,0)
+  //  ctx.translate(50,0)
     redraw();
 })
